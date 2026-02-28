9e22f2b9ad9165319fa71025ebb7b0e4d2e424b1
Scott Williams <scott@human.marketing>
2026-02-28 15:12:51 -0800

    initialize project

---
+++ src/ui/header/dropdown.tsx
@@ -0,0 +1,40 @@
+import type { LinkList, Page } from '@/sanity/types'
+import HoverDetails from '@/ui/hover-details'
+import SanityLink, { type SanityLinkType } from '@/ui/sanity-link'
+
+export default function ({
+	link: summary,
+	links,
+	_key,
+}: LinkList & { _key: string }) {
+	const anchorName = `--dropdown-${_key}`
+
+	return (
+		<HoverDetails
+			name="header"
+			className="accordion group/dropdown relative"
+			safeAreaOnHover
+		>
+			<summary
+				className="grid h-full group-open/dropdown:max-md:font-bold md:place-content-center md:text-center md:text-balance"
+				style={{ anchorName }}
+			>
+				{summary?.label || (summary?.internal as unknown as Page)?.title}
+			</summary>
+
+			<ul
+				className="md:bg-background anim-fade-to-b border-stroke max-md:pl-ch mb-ch md:-ml-ch z-1 max-md:border-l md:fixed md:min-w-max md:p-3 md:shadow-lg"
+				style={{
+					positionAnchor: anchorName,
+					positionArea: 'end span-end',
+				}}
+			>
+				{links?.map((link, key) => (
+					<li key={key}>
+						<SanityLink link={link as SanityLinkType} className="link" />
+					</li>
+				))}
+			</ul>
+		</HoverDetails>
+	)
+}
---
+++ src/ui/header/navigation.tsx
@@ -0,0 +1,40 @@
+import { getSite } from '@/sanity/lib/queries'
+import type { LinkList, Megamenu as MegamenuType } from '@/sanity/types'
+import SanityLink, { type SanityLinkType } from '@/ui/sanity-link'
+import Dropdown from './dropdown'
+import Megamenu from './megamenu'
+
+export default async function () {
+	const site = await getSite()
+
+	return (
+		<nav className="max-md:header-not-open:hidden max-md:anim-fade-to-b flex gap-x-4 [grid-area:navigation] max-md:my-4 max-md:flex-col">
+			{site?.header?.items?.map((item) => {
+				switch (item._type) {
+					case 'link':
+						return (
+							<SanityLink
+								link={item as SanityLinkType}
+								className="link grid md:place-content-center md:text-center"
+								key={item._key}
+							/>
+						)
+
+					case 'link.list':
+						return (
+							<Dropdown
+								{...(item as LinkList & { _key: string })}
+								key={item._key}
+							/>
+						)
+
+					case 'megamenu':
+						return <Megamenu {...(item as MegamenuType)} key={item._key} />
+
+					default:
+						return null
+				}
+			})}
+		</nav>
+	)
+}
